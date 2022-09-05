4a068f84e9ebb2e1aff77976cf1000da3122c81d
Your Name <you@example.com>
2022-09-05 10:42:27 +0100

    omo

--- pages/feed/[cat].js
+++ pages/feed/[cat].js
@@ -5,13 +5,12 @@ import TopicFilterComponent from '../../components/topicFilterComponent/topicFil
 
 const Feed = () => {
     const router = useRouter()
-    const cat =  router.query.cat
 
   return (
     <div>
       <div>
         <h1>{cat}</h1>
-        <TopicFilterComponent cat={cat} />
+        <TopicFilterComponent />
       </div>
       <div className=' grid grid-cols-1 md:grid-cols-2 content-center justify-items-center '>
         <TopicCardComponent />
